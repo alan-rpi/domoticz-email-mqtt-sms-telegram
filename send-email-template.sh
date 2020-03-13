6b5152ba2fe00c7f6f22641ea0a34a8c4a23e953
Alan, UK <25422008+alan-rpi@users.noreply.github.com>
2020-03-13 23:36:52 +0000

    replaced by yml file

--- send-email-template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Set the path to the script. See the README for using a directory within the Domoticz container. 
-# The default /config/ presumes you have stored the python script within the mapped directory.
-# The default below presumes the python script has been renamed from send-email-template.py
-python3 /config/send-email.py "$@"
